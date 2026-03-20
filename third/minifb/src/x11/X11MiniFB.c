84c64dc3af589f172653a2711126cd4e847d08c4
GFigure <qiuzhengyu@siggraph.org>
2026-03-20 23:45:29 +0800

    Added Linux timer

--- third/minifb/src/x11/X11MiniFB.c
+++ third/minifb/src/x11/X11MiniFB.c
@@ -992,3 +992,49 @@ mfb_show_cursor(struct mfb_window *window, bool show) {
         XDefineCursor(window_data_specific->display, window_data_specific->window, window_data_specific->invis_cursor);
     }
 }
+
+
+// =============================================================================
+// Linux Timer Implementation
+// =============================================================================
+#if defined(__linux__) || defined(linux)
+
+#ifndef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 199309L  // for clock_gettime, CLOCK_MONOTONIC
+#endif
+
+#include <time.h>
+
+extern double   g_timer_frequency;
+extern double   g_timer_resolution;
+
+#define kClock      CLOCK_MONOTONIC
+
+uint64_t
+mfb_timer_tick() {
+    struct timespec time;
+
+    if (clock_gettime(kClock, &time) != 0) {
+        return 0;
+    }
+
+    return (uint64_t)(time.tv_sec * 1e+9 + time.tv_nsec);
+}
+
+void
+mfb_timer_init() {
+    struct timespec res;
+
+    if (clock_getres(kClock, &res) != 0) {
+        g_timer_frequency = 1e+9;
+    }
+    else {
+        g_timer_frequency = res.tv_sec + res.tv_nsec * 1e-9;
+        if (g_timer_frequency == 0) {
+            g_timer_frequency = 1e+9;
+        }
+    }
+    g_timer_resolution = 1.0 / g_timer_frequency;
+}
+
+#endif // defined(__linux__) || defined(linux)
